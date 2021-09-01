fdd70d395eec3cc81991573f2a433dd6606972cc
EkimozSupport <88406196+EkimozSupport@users.noreply.github.com>
2021-09-01 04:05:52 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,26 +7,26 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""I am an open-source @CallsMusic bot, I let you play music in your group's voice chat.
+        f"""I am an open-source @EllycarlMusicbot, I let you play music in your group's voice chat.
 
 The commands I currently support are:
 
-/play - play the replied audio file or YouTube video
-/pause - pause the audio stream
-/resume - resume the audio stream
-/skip - skip the current audio stream
-/mute - mute the userbot
-/unmute - unmute the userbot
-/stop - clear the queue and remove the userbot from the call
+/play - yanıtlanan ses dosyasını veya YouTube videosunu oynatın
+/pause - ses akışını duraklat
+/resume - ses akışını devam ettir
+/skip - mevcut ses akışını atla
+/mute - kullanıcı robotunu sessize alır
+/unmute - userbot'un sesini açar
+/stop - kuyruğu temizleyin ve kullanıcı robotunu aramadan çıkarın
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/callsmusicchat"
+                        "Group", url="https://t.me/Smailesi"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/callsmusic"
+                        "Channel", url="https://t.me/Kizilsancakbilgi"
                     )
                 ]
             ]
